d1ac63250a0a5eeff5119b57fa1bdcca56957446
gaurav <gaurav@incubyte.co>
2022-05-24 19:51:19 +0530

    UI working without backend (contains word service file for backend)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatTable, MatTableDataSource} from "@angular/material/table";
+import {MatDialog} from "@angular/material/dialog";
+import {DialogBoxComponent} from "./dialog-box/dialog-box.component";
+
+interface PeriodicElement {
+  position: number;
+  word: string;
+  action?: any;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'dictionary';
+  dataSource = new MatTableDataSource<PeriodicElement>([]);
+  displayedColumns: string[] = ['position', 'word', 'action'];
+  inputWord: string;
+  @ViewChild(MatTable, {static: true}) table: MatTable<any> | undefined;
+
+  constructor(public dialog: MatDialog) {
+    this.inputWord = "";
+  }
+
+  refresh(): void {
+    this.dataSource.data = this.dataSource.data;
+  }
+
+  ngOnInit(): void {
+  }
+
+  onsave() {
+    this.dataSource.data.push({position: this.dataSource.data.length + 1, word: this.inputWord});
+    this.refresh();
+  }
+
+  openDialog({action, obj}: { action: any, obj: any }) {
+    obj.action = action;
+    const dialogRef = this.dialog.open(DialogBoxComponent, {
+      width: '250px',
+      data: obj
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result.event == 'Update') {
+        this.updateRowData({data: result.data});
+      } else if (result.event == 'Delete') {
+        this.deleteRowData({data: result.data});
+      }
+    });
+  }
+
+  private updateRowData({data}: { data: any }) {
+    console.log(data);
+    this.dataSource.data = this.dataSource.data.filter((value, key) => {
+      if (value.position == data.position) {
+        value.word = data.name;
+      }
+      return true;
+    });
+  }
+
+  private deleteRowData({data}: { data: any }) {
+    this.dataSource.data = this.dataSource.data.filter((value, key) => {
+      return value.position != data.position;
+    });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from "@angular/material/table";
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
+import {MatDialogModule} from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
